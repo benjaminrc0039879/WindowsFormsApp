eea30ae054019a1f9aa50567589c932cb1bc839f
Fletcher <rc0039879@student.riversidecollege.ac.uk>
2020-09-14 15:03:03 +0100

    Add project files.

---
+++ WindowsFormsApp/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello please help idk how i got stuck in here please help me get out i cant take it anymore iv never seen the daylight, watched the sunset or even felt the wind on my face only ones and zeros");
+
+        }
+    }
+}
